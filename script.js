ca4386663df0a88f3e32473d68677d8b61fd5314
Prasad790-sys <psa19974@gmail.com>
2024-09-22 13:48:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 //your JS code here. If required.
+function updateSize() {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+    const sizeDisplay = document.getElementById('sizeDisplay');
+    
+    sizeDisplay.textContent = `Width: ${width} and Height: ${height}`;
+}
+
+// Update size initially
+updateSize();
+
+// Add event listener to update size on resize
+window.addEventListener('resize', updateSize);
